4a7808915d19773cae3e8181688ce7a51ea90196
chihyeonWON <heungeob1003@gamil.com>
2022-02-04 15:11:39 +0900

    리스트박스의 다중선택 옵션 추가

--- tk_Listbox.py
+++ tk_Listbox.py
@@ -4,6 +4,7 @@ win.geometry("500x500")
 win.option_add("*Font", "Arial, 20")
 # Listbox
 lb = Listbox(win)
+lb.config(selectmode="multiple")
 lb.insert(0, "1번")
 lb.insert(1, "2번")
 lb.insert(2, "3번")
