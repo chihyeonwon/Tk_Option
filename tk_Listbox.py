7d8c1efaa6142d40dd8660beea0f2bf8fb793156
chihyeonWON <heungeob1003@gamil.com>
2022-02-04 15:23:41 +0900

    옵션을 선택했을 때 라벨에 몇 번째 옵션을 선택했는지 출력하는 기능 추가

--- tk_Listbox.py
+++ tk_Listbox.py
@@ -4,7 +4,7 @@ win.geometry("500x500")
 win.option_add("*Font", "Arial, 20")
 # Listbox
 lb = Listbox(win)
-lb.config(selectmode="multiple")
+# lb.config(selectmode="multiple")
 lb.insert(0, "1번")
 lb.insert(1, "2번")
 lb.insert(2, "3번")
@@ -15,6 +15,15 @@ lb.pack()
 # Button
 btn = Button(win)
 btn.config(text="옵션 선택")
+
+
+def click():
+    global text
+    text = lb.curselection()[0]
+    lab.config(text="{}번 째를 선택하셨습니다.".format(int(text)+1))
+
+
+btn.config(command=click)
 btn.pack()
 
 # Label
