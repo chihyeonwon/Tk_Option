277b3def127f1ef715d195bf85ebdb47b8a3ed26
chihyeonWON <heungeob1003@gamil.com>
2022-02-04 15:59:50 +0900

    옵션을 여러개 생성

--- tk_Radiobutton.py
+++ tk_Radiobutton.py
@@ -4,8 +4,12 @@ win.geometry("500x500")
 win.option_add("*Font", "Arial, 20")
 # Radiobutton
 rv = IntVar()
-rb = Radiobutton(win, text="1번", variable=rv)
-rb.pack()
+rb1 = Radiobutton(win, text="1번", variable=rv)
+rb2 = Radiobutton(win, text="2번", variable=rv)
+rb3 = Radiobutton(win, text="3번", variable=rv)
+rb1.pack()
+rb2.pack()
+rb3.pack()
 
 # Button
 btn = Button(win)
