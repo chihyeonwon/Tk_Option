555ccc560e1ba494c713ddf260086e23c278c789
chihyeonWON <heungeob1003@gamil.com>
2022-02-05 15:55:47 +0900

    first commit

---
+++ tk_Combobox.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter .ttk import *
+win = Tk()
+win.geometry("500x500")
+win.option_add("*Font", "Arial, 20")
+# Button
+btn = Button(win)
+btn.config(text="옵션 선택")
+
+
+def click():
+    lab_text = rv.get()
+    lab.config(text=lab_text)
+
+
+btn.config(command=click)
+btn.pack()
+
+# Label
+lab = Label(win)
+lab.pack()
+win.mainloop()
