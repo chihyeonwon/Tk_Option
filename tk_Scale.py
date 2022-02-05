67950d6125924c1e68d427ec74e192484d3f0983
chihyeonWON <heungeob1003@gamil.com>
2022-02-05 16:35:34 +0900

    get함수를 사용해 수치를 출력

--- tk_Scale.py
+++ tk_Scale.py
@@ -4,7 +4,7 @@ win.geometry("500x500")
 win.option_add("*Font", "Arial, 20")
 # Scale
 scale = Scale(win)
-scale.config(from_=0, to=50, orient="horizontal")
+scale.config(length=1000, tickinterval=10, from_=0, to=50, orient="horizontal")
 scale.pack()
 
 # Button
@@ -13,7 +13,7 @@ btn.config(text="옵션 선택")
 
 
 def click():
-    lab_text = sb.get()
+    lab_text = scale.get()
     lab.config(text=lab_text)
 
 
