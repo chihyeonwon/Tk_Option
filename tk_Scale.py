0396127dd4b0ee62de7d46568ed265d5d4a5940f
chihyeonWON <heungeob1003@gamil.com>
2022-02-05 16:28:22 +0900

    config의 from_, to 옵션으로 범위 설정

--- tk_Scale.py
+++ tk_Scale.py
@@ -2,6 +2,11 @@ from tkinter import *
 win = Tk()
 win.geometry("500x500")
 win.option_add("*Font", "Arial, 20")
+# Scale
+scale = Scale(win)
+scale.config(from_=50, to=0)
+scale.pack()
+
 # Button
 btn = Button(win)
 btn.config(text="옵션 선택")
